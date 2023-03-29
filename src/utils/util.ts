0fa24c490f3b226ddbd20fba67d261b89900dc46
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-03-30 01:14:39 +0530

    Add custom theme to tailwind config + Add utils/util.ts

---
+++ src/utils/util.ts
@@ -0,0 +1,9 @@
+import type { ClassValue } from "clsx";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
+
+export const randint = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
