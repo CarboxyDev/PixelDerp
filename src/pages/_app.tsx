ca33bec98e24082240cff00d85e3eb6509615c95
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-04-03 01:57:18 +0530

    Add next-auth integration to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import { trpc } from "../utils/trpc";
 import { type AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+const MyApp = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
