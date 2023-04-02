b44020aca62c3d371c74b85095e947c4e8373cc1
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-04-02 20:36:52 +0530

    Add supabase auth wrap in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
-import { type AppType } from "next/app";
-
 import { trpc } from "../utils/trpc";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import {
+  SessionContextProvider,
+  type Session,
+} from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { type AppProps } from "next/app";
 
 import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp = ({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) => {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,38 @@
 import React from "react";
+import { supabase } from "../server/db/auth";
 
 interface DashboardProps {}
 
 export async function getServerSideProps() {
+  const { data, error } = await supabase.auth.getSession();
+  console.log(data);
+  if (data.session === null) {
+  }
+
   return {
     props: {},
   };
 }
 
 export default function dashboard(props: DashboardProps) {
-  return <div>dashboard</div>;
+  async function logout() {
+    const error = await supabase.auth.signOut();
+    if (error) {
+      console.log(error);
+    }
+    window.location.href = "/login";
+  }
+
+  return (
+    <div>
+      <div>
+        <button
+          className="m-12 rounded-lg border border-primary bg-primary px-6 py-3 text-white transition duration-300 ease-in hover:border-primary hover:bg-transparent hover:text-primary active:scale-95"
+          onClick={() => logout()}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 }
